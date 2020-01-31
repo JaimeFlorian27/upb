1ca783af9a00e373adfcefe63fba0784b77495a9
JaimeFlorian27 <60119094+JaimeFlorian27@users.noreply.github.com>
2020-01-31 12:47:53 -0500

    Add files via upload

---
+++ Desafio 2-2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese su salario mensual para determinar su tarifa");
+        double salario = double.Parse(Console.ReadLine());
+
+        double smmlv = 877803;
+
+        if (salario< 2 * smmlv)
+        {
+            Console.WriteLine("Usted deberá cancelar la tarifa A");
+        }
+        else if (2*smmlv<= salario && salario < 4* smmlv)
+        {
+            Console.WriteLine("Usted deberá cancelar la tarifa B");
+        }
+        else
+        {
+            Console.WriteLine("Usted deberá cancelar la tarifa C");
+        }
+        }
+    }
---
+++ Desafio 3-2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese su salario mensual para determinar su tarifa");
+        double salario = double.Parse(Console.ReadLine());
+
+        double smmlv = 877803;
+
+        if (salario < 2 * smmlv)
+        {
+            double cuota = 3400;
+            Console.WriteLine("Usted deberá cancelar la siguiente suma: "+ cuota);
+        }
+        else if (2 * smmlv <= salario && salario < 5 * smmlv)
+        {
+            double cuota = 13500;
+            Console.WriteLine("Usted deberá cancelar la siguiente suma: " + cuota);
+        }
+        else
+        {
+            double cuota = 35600;
+            Console.WriteLine("Usted deberá cancelar la siguiente suma: " + cuota);
+        }
+    }
+}
